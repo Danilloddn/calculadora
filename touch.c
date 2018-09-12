61b0b04183f92585373c626973428fb2d2f01efa
Slorack <ra90561@uem.br>
2018-09-12 16:46:35 -0300

    main pronta

--- touch.c
+++ touch.c
@@ -10,10 +10,29 @@ double div(double x, double y){
 	if(y!=0) return x/y;
 	else return -1;
 }
-double div(double x, double y){
+double mul(double x, double y){
 	return x*y;
 }
 
 int main(){
+	int x,y,op;
+	printf("Digite num x e y:\n");
+	scanf("%d,%d", &x,&y);
+	printf("Entre com a opcao desejada \n
+	1-Soma
+	2-Subtracao
+	3-Divisao
+	4-Multiplicacao
+	/n);
+	scanf("%d", op);
+	switch(op){
+	case 1: soma(x,y);
+	break;
+	case 2: sub(x, y);
+	break;
+	case 3: div(x, y);
+	break;	
+	case 4: mul(x,y);
+	break;
 	return 0;
 }
